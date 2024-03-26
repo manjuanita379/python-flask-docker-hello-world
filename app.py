9c87dae7d028b105f369a6a82d1b6a4ea7c26cc0
manjuanita379 <145435792+manjuanita379@users.noreply.github.com>
2024-03-26 17:38:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker!!"
+    return "Hello World"
 
 
-if __name__ == "__main__":
+if __name__ == "__master__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True,host='0.0.0.0',port=port)
+    app.run(debug=True,host='54.151.2.1',port=5000)
